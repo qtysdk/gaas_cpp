14a8a13145186c250251b858dfab378da099f274
Ching Yi, Chan <qrtt1@infuseai.io>
2023-02-22 14:38:26 +0800

    Add logic from tutorial

--- NothingCtrl.h
+++ NothingCtrl.h
@@ -11,5 +11,7 @@ class NothingCtrl : public drogon::HttpSimpleController<NothingCtrl>
     PATH_LIST_BEGIN
     // list path definitions here;
     // PATH_ADD("/path", "filter1", "filter2", HttpMethod1, HttpMethod2...);
+    PATH_ADD("/",Get,Post);
+    PATH_ADD("/test",Get);
     PATH_LIST_END
 };
