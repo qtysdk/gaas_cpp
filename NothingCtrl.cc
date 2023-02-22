9ffcd24e910a116106c3e2669c125e81c5f9ebb9
Ching Yi, Chan <qrtt1@infuseai.io>
2023-02-22 14:28:21 +0800

    Codegen: NothingCtrl
    Signed-off-by: Ching Yi, Chan <qrtt1@infuseai.io>

---
+++ NothingCtrl.cc
@@ -0,0 +1,6 @@
+#include "NothingCtrl.h"
+
+void NothingCtrl::asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback)
+{
+    // write your application logic here
+}
---
+++ NothingCtrl.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#include <drogon/HttpSimpleController.h>
+
+using namespace drogon;
+
+class NothingCtrl : public drogon::HttpSimpleController<NothingCtrl>
+{
+  public:
+    void asyncHandleHttpRequest(const HttpRequestPtr& req, std::function<void (const HttpResponsePtr &)> &&callback) override;
+    PATH_LIST_BEGIN
+    // list path definitions here;
+    // PATH_ADD("/path", "filter1", "filter2", HttpMethod1, HttpMethod2...);
+    PATH_LIST_END
+};
